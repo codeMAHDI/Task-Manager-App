d994836792a9a1e78948b06e51a8711ca32f6660
Moshiur Rahman (Mahdi) <89650128+codeMAHDI@users.noreply.github.com>
2025-12-01 01:39:36 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
