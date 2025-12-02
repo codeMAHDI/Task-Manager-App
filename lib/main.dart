64b232f9c88d81ab72a9cef70289e78b906ed560
Moshiur Rahman (Mahdi) <89650128+codeMAHDI@users.noreply.github.com>
2025-12-03 03:27:44 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() async {
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
+
 class TaskCubit extends Cubit<List<Map<String, dynamic>>> {
   TaskCubit() : super([]);
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
